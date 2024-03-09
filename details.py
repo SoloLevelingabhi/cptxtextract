11d97018ef51165d3d942893ab498afd68f30ea3
Virendraverma9950 <153084063+Virendraverma9950@users.noreply.github.com>
2024-03-09 19:39:39 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
-auth_users = [1713924419]
-sudo_user = 1713924419
-log_channel = -1002143629860
-txt_channel = -1002049391187
+api_id = 25919081
+api_hash = "0bc2fdba14b16b44f0d89729ed8d2118"
+bot_token = "6745845610:AAHpc9FH6ZX99o4pJAmICUMpMfooNZX7l74"
+auth_users = [1826828317]
+sudo_user = 1826828317
+log_channel = -1002015954185
+txt_channel = -1002015954185
