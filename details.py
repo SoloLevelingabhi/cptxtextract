5a49d3d6d777c52fe0eb13c0d52d8154faaf47b0
Virendraverma9950 <153084063+Virendraverma9950@users.noreply.github.com>
2024-03-10 22:48:02 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,7 +2,7 @@ import os
 
 api_id = 25919081
 api_hash = "0bc2fdba14b16b44f0d89729ed8d2118"
-bot_token = "6745845610:AAHpc9FH6ZX99o4pJAmICUMpMfooNZX7l74"
+bot_token = "7199858392:AAGJleD_elmLYuZG2D52KUz3FqQzAY2ARzQ"
 auth_users = [1826828317]
 sudo_user = 1826828317
 log_channel = -1002015954185
